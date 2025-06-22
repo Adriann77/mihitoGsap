5cfca0502103fd179c3494074820bd3f8dbbbc78
AdrianDeweloperskie <adrian.klimas@deweloperskie.pl>
2025-06-22 18:09:21 +0200

    implement navbar & hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,82 @@
+import { useGSAP } from '@gsap/react';
+import { SplitText } from 'gsap/all';
+import gsap from 'gsap';
+import React from 'react';
+
+const Hero = () => {
+  useGSAP(() => {
+    const heroSplit = new SplitText('.title', { type: 'chars, words' });
+    const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
+
+    heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
+
+    gsap.from(heroSplit.chars, {
+      yPercent: 100,
+      duration: 1,
+      ease: 'expo.out',
+      stagger: 0.06,
+    });
+
+    gsap.from(paragraphSplit.lines, {
+      opacity: 0,
+      yPercent: 100,
+      duration: 1.8,
+      stagger: 0.06,
+      delay: 1,
+      ease: 'expo.out',
+    });
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: '#hero',
+          start: 'top top',
+          end: 'bottom top',
+          scrub: true,
+        },
+      })
+      .to('.right-leaf', { y: 200 }, 0)
+      .to('.left-leaf', { y: -200 }, 0);
+  }, []);
+  return (
+    <>
+      <section
+        id='hero'
+        className='noisy'
+      >
+        <h1 className='title'>Mohito</h1>
+
+        <img
+          src='/images/hero-left-leaf.png'
+          alt='left-leaft'
+          className='left-leaf'
+        ></img>
+        <img
+          src='/images/hero-right-leaf.png'
+          alt='right-leaft'
+          className='right-leaf'
+        ></img>
+
+        <div className='body'>
+          <div className='content'>
+            <div className='"space-y-5'>
+              <p>Cool. Crisp. Classic.</p>
+              <p className='subtitle'>
+                Sip the Spirit <br></br> of Summer
+              </p>
+            </div>
+            <div className='view-cocktails'>
+              <p className='subtitle'>
+                Every cocktail on our menu is a blender of premnium liquiers
+                designer to dlight your senses
+              </p>
+              <a href='#cocktails'>View Cocktails</a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
